8f3f1d153ccda9b6e7d9a0031a1ed0fd32281fa2
Steven Liao <stevenliao19900326@gmail.com>
2019-06-29 15:10:49 +1000

    first commit

---
+++ build/precache-manifest.0ea4e08edb4cd4994c1b6bb0ff3df753.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fa09d18590b6a1d6c4c",
+    "url": "/static/css/4.dc680ad7.chunk.css"
+  },
+  {
+    "revision": "0acef3da4721e86f01ce",
+    "url": "/static/css/main.be3a5df4.chunk.css"
+  },
+  {
+    "revision": "bd53ac976681dc53a3c6",
+    "url": "/static/js/runtime~main.6c2a4d32.js"
+  },
+  {
+    "revision": "ef8274f6e30812151381",
+    "url": "/static/js/2.83a057d0.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "2c57f67a5ee71fd1efd3",
+    "url": "/static/js/3.6d8f39c3.chunk.js"
+  },
+  {
+    "revision": "0acef3da4721e86f01ce",
+    "url": "/static/js/main.1689dbd8.chunk.js"
+  },
+  {
+    "revision": "4fa09d18590b6a1d6c4c",
+    "url": "/static/js/4.c010a500.chunk.js"
+  },
+  {
+    "revision": "34c9f862184952b9d104",
+    "url": "/static/js/5.cfb03230.chunk.js"
+  },
+  {
+    "revision": "34c9f862184952b9d104",
+    "url": "/static/css/5.5af80f7a.chunk.css"
+  },
+  {
+    "revision": "2c57f67a5ee71fd1efd3",
+    "url": "/static/css/3.70af9a39.chunk.css"
+  },
+  {
+    "revision": "a61ff57ac5410d335836d4b3faf94277",
+    "url": "/index.html"
+  }
+];
